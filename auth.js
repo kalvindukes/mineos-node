036d473abd24ac3b6989a9a6f448850766ab8204
William Dizon <wdchromium@gmail.com>
2014-12-05 12:13:00 -0700

    auth.authenticate_shadow_user created

--- auth.js
+++ auth.js
@@ -30,3 +30,26 @@ auth.get_group_owner = function(path, callback) {
     callback(err, posix.getgrnam(stat_info['gid'])['name']);
   })
 }
+
+auth.authenticate_shadow_user = function(user, plaintext, callback) {
+  var passwd = require('etc-passwd');
+  var hash = require('sha512crypt-node');
+
+  passwd.getShadow({username:user}, function(err, shadow_info) { 
+    if (err) {
+      callback(true, false);
+    } else {
+      try {
+        var password_parts = shadow_info['password'].split(/\$/);
+        var salt = password_parts[2];
+        var new_hash = hash.sha512crypt(plaintext, salt);
+
+        callback(err, new_hash == shadow_info['password']);
+      } catch (e) {
+        callback(true, false);
+      }
+      
+    }
+      
+  })
+}
--- test/test-auth.js
+++ test/test-auth.js
@@ -69,3 +69,32 @@ test.get_group_owner = function(test) {
     test.done();
   })
 }
+
+test.authenticate_shadow_user = function(test) {
+  //this test depends on the existence of system user "weak" with password "password"
+  async.series([
+    function(callback) {
+      auth.authenticate_shadow_user('weak', 'password', function(err, authed) {
+        test.ifError(err);
+        test.ok(authed);
+        callback(err);
+      })
+    },
+    function(callback) {
+      auth.authenticate_shadow_user('weak', 'notthepassword', function(err, authed) {
+        test.ifError(err);
+        test.ok(!authed);
+        callback(err);
+      })
+    },
+    function(callback) {
+      auth.authenticate_shadow_user('fake', 'notthepassword', function(err, authed) {
+        test.ok(err);
+        test.ok(!authed);
+        callback(err);
+      })
+    }
+  ], function(err, results) {
+    test.done();
+  })
+}
